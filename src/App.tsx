4ccea0cc58dc0ca8e9c7a6815c974dcd12fe7c40
Pavel Petrov <104124867+PashaPP@users.noreply.github.com>
2023-02-15 19:00:55 +0200

    1 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
 import './App.css';
+import { TaskType, TodoList } from './components/Todolist';
 
-function App() {
+export function App() {
+
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: "CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false }
+  ]
+
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: "Lost", isDone: true },
+    { id: 2, title: "Home", isDone: false },
+    { id: 3, title: "XXX", isDone: true }
+  ]
+  let tasks3: Array<TaskType> = [
+    { id: 1, title: "Tennis", isDone: false },
+    { id: 2, title: "CF", isDone: true },
+    { id: 3, title: "Hockey", isDone: false }
+  ]
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        Learn React
-      </header>
+      <TodoList title="What to learn" tasks={tasks1} />
+      <TodoList title="Movies" tasks={tasks2} />
+      <TodoList title="Sport" tasks={tasks3} />
     </div>
   );
 }
 
-export default App;
